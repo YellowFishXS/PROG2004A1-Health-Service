aaab641d33555694e00e02e3d7ea91c086b6f75d
YellowFishXS <133674460+YellowFishXS@users.noreply.github.com>
2025-11-04 17:34:18 +0800

    Update AssignmentOne.java

--- src/AssignmentOne.java
+++ src/AssignmentOne.java
@@ -1,7 +1,10 @@
 public class AssignmentOne {
     public static void main(String[] args) {
-        HealthProfessional h1 = new HealthProfessional(123,"abc",35);
-        System.out.println(h1.toString());
-        h1.printHealthProfessional();
+//        HealthProfessional h1 = new HealthProfessional(123,"abc",35);
+//        System.out.println(h1.toString());
+//        h1.printHealthProfessional();
+
+        GeneralPractitioner g1 = new GeneralPractitioner();
+        g1.printHealthProfessional();
     }
 }

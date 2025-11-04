470f09e61a8b6926e7d3dedf9881c03c25349480
YellowFishXS <133674460+YellowFishXS@users.noreply.github.com>
2025-11-04 17:33:39 +0800

    Create GeneralPractitioner.java
    part 2 General Practitioner class

---
+++ src/GeneralPractitioner.java
@@ -0,0 +1,49 @@
+import jdk.nashorn.internal.objects.annotations.Getter;
+
+/**
+ * Child class GeneralPractitioner extends base class
+ */
+public class GeneralPractitioner extends HealthProfessional {
+    //Distinguish general practitioners from other types
+    private boolean isGeneralPractitioner;
+
+    /**
+     * default constructor
+     */
+    public GeneralPractitioner() {
+        super();
+        this.isGeneralPractitioner = true;
+        System.out.println("A default general practitioner object has been created");
+    }
+
+    public GeneralPractitioner(int id, String name, int Age, boolean isGeneralPractitioner){
+        // Call the constructor of the base class
+        super(id, name, Age);
+        this.isGeneralPractitioner = isGeneralPractitioner;
+        System.out.println("created GeneralPractitioner name: " + name);
+    }
+
+    /**
+     * overriding the method of the parent class
+     */
+    @Override
+    public void printHealthProfessional() {
+        System.out.println("=== General Practitioner Detailed information===");
+        System.out.println("ID: " + getId());
+        System.out.println("Name: " + getName());
+        System.out.println("Age: " + getAge());
+        if (isGeneralPractitioner) {
+            System.out.println("He/She is a general practitioner");
+        } else
+            System.out.println("He/She is not a general practitioner");
+        System.out.println("========================");
+    }
+
+    public boolean isGeneralPractitioner() {
+        return isGeneralPractitioner;
+    }
+
+    public void setGeneralPractitioner(boolean generalPractitioner) {
+        isGeneralPractitioner = generalPractitioner;
+    }
+}

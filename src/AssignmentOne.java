2b979b09055c4ecf01088078f020ee3fc36a037f
YellowFishXS <133674460+YellowFishXS@users.noreply.github.com>
2025-11-04 10:53:56 +0800

    base class

--- src/AssignmentOne.java
+++ src/AssignmentOne.java
@@ -1,5 +1,7 @@
 public class AssignmentOne {
     public static void main(String[] args) {
-
+        HealthProfessional h1 = new HealthProfessional(123,"abc");
+        System.out.println(h1.toString());
+        System.out.println("id"+ h1.getId() + ", name:"+h1.getName());
     }
 }
--- src/HealthProfessional.java
+++ src/HealthProfessional.java
@@ -1,2 +1,57 @@
+/**
+ * HealthProfessional - base class
+ */
 public class HealthProfessional {
+    private int id;
+    private String name;
+
+    public HealthProfessional() {
+        this.id = 0;
+        this.name = "";
+        System.out.println("create HealthProfessional");
+    }
+
+    /**
+     *  Constructor for objects of class HealthProfessional
+     * @param id
+     * @param name
+     */
+    public HealthProfessional(int id, String name) {
+        this.id = id;
+        this.name = name;
+        System.out.println("Constructor--HealthProfessional: " + name + " (ID: " + id + ")");
+    }
+
+    //access the properties
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    //manipulate the properties
+    public boolean setId(int id) {
+        //perform input validation
+        if (id>0){
+            this.id = id;
+        }else{
+            System.out.println("input invalidation");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean setName(String name) {
+        //perform input validation
+        if ((name.length()>3) && (name.length()<10)){
+            this.name = name;
+        }else {
+            System.out.println("input invalidation");
+            return false;
+        }
+        return true;
+    }
+
 }

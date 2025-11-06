31c7a4c735aca7bba50827eaf7c154c42c14bf8e
YellowFishXS <133674460+YellowFishXS@users.noreply.github.com>
2025-11-06 22:38:27 +0800

    Update Appointment.java

--- src/Appointment.java
+++ src/Appointment.java
@@ -1,2 +1,28 @@
 public class Appointment {
+    private String name;
+    private String mobilePhone;
+    private String preferredTime;
+
+    private HealthProfessional selectedDoctor;
+
+    public Appointment() {
+        //default constrator
+    }
+
+    public Appointment(String name, String mobilePhone, String preferredTime, HealthProfessional selectedDoctor) {
+        this.name = name;
+        this.mobilePhone = mobilePhone;
+        this.preferredTime = preferredTime;
+        this.selectedDoctor = selectedDoctor;
+    }
+
+    public void printDetail() {
+        System.out.println("The appointment details are:");
+        System.out.println("Name: " + this.name);
+        System.out.println("Phone Number: " + this.mobilePhone);
+        System.out.println("===========Doctor Detail==========");
+        this.selectedDoctor.printHealthProfessional();
+    }
+
+
 }
